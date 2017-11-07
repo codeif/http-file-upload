c717099bc25d49ddee9fb94b3bf2d1c0c36a6099
codeif <me@codeif.com>
2017-11-07 12:21:11 +0800

    1.默认端口8888 2.可以在命令行设置端口 3.添加.gitignore

--- server/file_upload.py
+++ server/file_upload.py
@@ -60,5 +60,6 @@ def upload_file():
 
     return 'test'
 
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=9877)
+    app.run(host='0.0.0.0', port=8888)
--- server/tornado_app.py
+++ server/tornado_app.py
@@ -1,11 +1,24 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
 from tornado.wsgi import WSGIContainer
 from tornado.httpserver import HTTPServer
 from tornado.ioloop import IOLoop
+from tornado.options import define, options, parse_command_line
+
 from file_upload import app
 
-http_server = HTTPServer(WSGIContainer(app))
-http_server.listen(9877)
-IOLoop.instance().start()
+
+define("port", default=8888, help="run on the given port", type=int)
+define("", default=8888, help="run on the given port", type=int)
+
+
+def main():
+    parse_command_line()
+    http_server = HTTPServer(WSGIContainer(app))
+    http_server.listen(options.port)
+    print('Listening on http://0.0.0.0:%d' % options.port)
+    IOLoop.instance().start()
+
+
+if __name__ == '__main__':
+    main()
